397e4dfc949fc8cae091e84380c7ceecae6cd8b8
Gandhi Rajan Natarajan <00002678@arisglobal.com>
2018-07-22 14:07:20 +0530

    Minimum swap count finder class

---
+++ MinimumStringSwapFinder.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinimumStringSwapFinder {
+    public static void main(String args[]) {
+        List<String> inputStringList = new ArrayList<>();
+        inputStringList.add("Roger");
+        inputStringList.add("Federer");
+        inputStringList.add("Rahul");
+        inputStringList.add("Rahul");
+        inputStringList.add("Roger");
+        inputStringList.add("Federer");
+        System.out.println(findMinimumAdjacentSwaps(inputStringList));
+    }
+
+
+    private static  int findMinimumAdjacentSwaps(List<String> inputStringList) {
+        List<String> visitedList = new ArrayList<>();
+        int minimumSwaps = 0;
+        for (int i = 0; i < inputStringList.size(); i++) {
+            if (!visitedList.contains(inputStringList.get(i))) {
+                visitedList.add(inputStringList.get(i));
+
+                int count = 0;
+                for (int j = i + 1; j < inputStringList.size(); j++) {
+                   if (!visitedList.contains(inputStringList.get(j)))
+                        count++;
+
+                    else if (inputStringList.get(i).equals(inputStringList.get(j)))
+                        minimumSwaps += count;
+                }
+            }
+        }
+        return minimumSwaps;
+    }
+}
